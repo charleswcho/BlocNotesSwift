16d298c813a24519fdbddc5dd438d3479ed9a721
Charles Wesley Cho <charleswcho@gmail.com>
2015-07-30 15:37:17 -0700

    Setup core data

--- BlocNotes/BlocNotes/AppDelegate.swift
+++ BlocNotes/BlocNotes/AppDelegate.swift
@@ -49,11 +49,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     // MARK: - Core Data stack
     
-    lazy var applicationDocumentsDirectory: NSURL = {
-        // The directory the application uses to store the Core Data store file. This code uses a directory named "com.charleswesleycho.BlocNotes" in the application's documents Application Support directory.
-        let urls = NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)
-        return urls[urls.count-1] as! NSURL
-    }()
+    lazy var applicationDocumentsDirectory: NSURL? = {
+        return NSFileManager.defaultManager().containerURLForSecurityApplicationGroupIdentifier("com.charleswesleycho.BlocNotes") ?? nil
+        }()
 
     lazy var managedObjectModel: NSManagedObjectModel = {
         // The managed object model for the application. This property is not optional. It is a fatal error for the application not to be able to find and load its model.
@@ -65,7 +63,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // The persistent store coordinator for the application. This implementation creates and return a coordinator, having added the store for the application to it. This property is optional since there are legitimate error conditions that could cause the creation of the store to fail.
         // Create the coordinator and store
         var coordinator: NSPersistentStoreCoordinator? = NSPersistentStoreCoordinator(managedObjectModel: self.managedObjectModel)
-        let url = self.applicationDocumentsDirectory.URLByAppendingPathComponent("BlocNotes.sqlite")
+        let url = self.applicationDocumentsDirectory!.URLByAppendingPathComponent("BlocNotes.sqlite")
         var error: NSError? = nil
         var failureReason = "There was an error creating or loading the application's saved data."
         
